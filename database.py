bb97e4fca95046e9d2e9a04586a720b324d15538
gayathrisunil <56356145+gayathrisunil@users.noreply.github.com>
2022-03-26 19:42:41 +0530

    db test

--- database.py
+++ database.py
@@ -1,3 +1,21 @@
+import pymongo
 from pymongo import MongoClient
 
-# handle DB requests using MongoClient
+#usrname= "gayathri-sunil"
+#pass="NySZ8OCAEE1uQZap"
+
+CONN_STR = "mongodb+srv://gayathri-sunil:NySZ8OCAEE1uQZap@cluster0.mn5ub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = MongoClient(CONN_STR)
+
+db = client["ride-share-db"]
+collection= db["ride-share"]
+
+#need to change this later to get it from local host
+doc= {  "custID": 28749387,
+		"pickup": "mypickuploc",
+		"destination": "mydroploc",
+		"time": "2022-03-26T11:30:05",
+		"cost": 120,
+		"seats": 2}
+collection.insert_one(doc)
+
