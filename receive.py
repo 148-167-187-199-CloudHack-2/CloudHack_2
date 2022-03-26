f3a6de7015a1dc4d9df201969c93953a9b0cd112
gayathrisunil <56356145+gayathrisunil@users.noreply.github.com>
2022-03-26 21:03:50 +0530

    sending and receiving ride sharing details

---
+++ receive.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import pika, sys, os
+import time
+import tqdm
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='new_ride_queue')
+
+    def callback(ch, method, properties, body):
+        val_passed= body.decode()
+        time_to_sleep= int(val_passed.split(' ')[2])
+        print("Microservice sleeping for %r seconds" % time_to_sleep)
+        #time.sleep(time_to_sleep)
+        for i in tqdm.tqdm(range(time_to_sleep)):
+            time.sleep(0.01)
+        print()
+        print("Pickup location: ", val_passed.split(' ')[0])
+        print("Drop location: ", val_passed.split(' ')[1])
+        print("Cost: ", val_passed.split(' ')[3])
+        print("Seats: ", val_passed.split(' ')[4])
+
+    channel.basic_consume(queue='new_ride_queue', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ send.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import pika
+from datetime import datetime
+import sys
+# now = datetime.now()
+# time = now.strftime("%H:%M:%S")
+
+message = ''.join(sys.argv[1])
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='new_ride_queue')
+
+channel.basic_publish(exchange='', routing_key='new_ride_queue', body=message)
+print(" [x] Sent time to receiver")
+connection.close()
