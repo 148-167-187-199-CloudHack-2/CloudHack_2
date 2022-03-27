b4be9671c05839be38f5a045f42e4024e7041175
gayathrisunil <56356145+gayathrisunil@users.noreply.github.com>
2022-03-27 19:55:25 +0530

    consumer updated

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,31 +1,21 @@
 import pika
-
 import os
 import time
 
 # set these via the Dockerfile
 SERVER_IP = os.environ["SERVER_IP"]
 SERVER_PORT = os.environ["SERVER_PORT"]
+CUST_ID= os.environ["CUST_ID"]
 
-def on_open(connection):
-    connection.channel(on_open_callback=on_channel_open)
-
-def on_channel_open(channel):
-	# define this using the pika interface.
-	pass
-
-def get_time_from_rabbit(connection):
-	# Set up RabbitMQ client here with pika.
-
-	# Unsure how to do this. Adding a link that may help:
-	## https://github.com/jay-johnson/kombu-and-pika-pub-sub-examples/tree/master/pika
-
-	return time # return an int pls
+def get_time_from_rabbit(ch, method, properties, body):
+	val_passed = body.decode()
+	time_to_sleep = int(val_passed)
+	print("Sleeping for %r seconds", time_to_sleep)
+	time.sleep(time_to_sleep) 
 
 if __name__ == '__main__':
-	connection = pika.SelectConnection(on_open_callback=on_open)
-
-	time = get_time_from_rabbit(connection)
-	time.sleep(time)
-
-	# this may need refactoring; the consumers are a continuous looping process.
+	connection= pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+	channel=connection.channel()
+	channel.queue_declare(queue='new_ride_queue')
+	channel.basic_consume(queue='new_ride_queue', on_message_callback=get_time_from_rabbit, auto_ack=True)
+	channel.start_consuming()
