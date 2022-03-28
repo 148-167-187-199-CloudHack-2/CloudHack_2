b1a2953b65591c74747749b3f58d8a60f07e5312
Hrishit Chaudhuri <hrishitchaudhuri@gmail.com>
2022-03-28 11:37:54 +0530

    god bless us everyone once again

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -10,10 +10,8 @@ CUST_ID= os.environ["CUST_ID"]
 def get_time_from_rabbit(ch, method, properties, body):
 	val_passed = body.decode()
 	time_to_sleep = int(val_passed)
-	print("Sleeping for %r seconds", time_to_sleep)
+	print("Sleeping for ", time_to_sleep, " seconds")
 	time.sleep(time_to_sleep) 
-
-	ch.basic_ack(delivery_tag=method.delivery_tag)
 
 if __name__ == '__main__':
 	time.sleep(60)
@@ -23,6 +21,7 @@ if __name__ == '__main__':
 		channel=connection.channel()
 	
 		channel.queue_declare(queue='new_ride_queue')
+		print("Helo")
 
 		channel.basic_qos(prefetch_count=1)
 		channel.basic_consume(queue='new_ride_queue', on_message_callback=get_time_from_rabbit, auto_ack=True)
--- producer/producer.py
+++ producer/producer.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pika
+import time
 
 # use pika for RabbitMQ connections
 
@@ -7,18 +8,17 @@ app = Flask(__name__)
 
 @app.route("/new_ride", methods=['POST'])
 def handle_new_ride():
-	time = request.form['time']
-
 	try:
 		connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
 	except pika.exceptions.AMQPConnectionError as exc:
 		print("Failed to connect to RabbitMQ service. Message wont be sent.")
-		return
 
 	channel=connection.channel() 
+	
+	time = request.form['time']
 
 	# push time to RabbitMQ client.
-	channel.queue_declare(queue='new_ride_queue', durable=True)
+	channel.queue_declare(queue='new_ride_queue')
 	channel.basic_publish(
 		exchange='',
 		routing_key='new_ride_queue',
@@ -29,26 +29,25 @@ def handle_new_ride():
 
 	connection.close()
 
-	return "Sent message" + time + "to receiver"
+	return "Sent message" + time + "to receiver\n"
 
 
 @app.route("/new_ride_matching_customer", methods=['POST'])
 def handle_new_customer():
-	customer = jsonify(request.form)
-
 	try:
 		connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
 	except pika.exceptions.AMQPConnectionError as exc:
 		print("Failed to connect to RabbitMQ service. Message wont be sent.")
-		return
 
 	channel=connection.channel() 
+	customer = jsonify(request.form)
+	customer = str(customer)
 
-	channel.queue_declare(queue='new_ride_matching_queue', durable=True)
+	channel.queue_declare(queue='new_ride_matching_queue')
 	channel.basic_publish(
 		exchange='',
 		routing_key='new_ride_matching_queue',
-		body=time,
+		body=customer,
 		properties=pika.BasicProperties(
 			delivery_mode=2,  # make message persistent
 		))
